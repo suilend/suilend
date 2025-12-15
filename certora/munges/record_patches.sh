d7b6f07e44d71e31ab9aab7d456500787b825d38
Kevin Lotz <kevin@certora.com>
2025-12-15 17:42:37 +0100

    chore: update patches

---
+++ certora/munges/record_patches.sh
@@ -0,0 +1,3 @@
+git diff HEAD:contracts/suilend/sources/obligation.move  contracts/suilend/sources/obligation.move  > certora/munges/obligation.patch;
+git diff HEAD:contracts/suilend/sources/lending_market.move  contracts/suilend/sources/lending_market.move  > certora/munges/lending_market.patch;
+git diff HEAD:contracts/suilend/sources/reserve.move  contracts/suilend/sources/reserve.move  > certora/munges/reserve.patch;
